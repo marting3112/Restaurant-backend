fee48c66e6d5069c44268c60f084bf863f9296ea
marting3112 <mgonzalez100247@umanizales.edu.co>
2024-02-22 16:18:41 -0500

    se conectó a la base de datos

--- src/DataBase/connection.js
+++ src/DataBase/connection.js
@@ -1,8 +1,8 @@
 const {sequelize, Sequelize} = require ('sequelize');
 
 var dataBase = 'restaurantDB';
-var userName = 'postgress';
-var password = '123456789';
+var userName = 'postgres';
+var password = 'martin3112';
 
 const connection = new Sequelize(dataBase, userName,password,{
     host: 'localhost',
